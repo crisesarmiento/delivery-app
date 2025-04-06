044a8330317df37be6353a268843d4a209d77936
Cristian Sarmiento <cristian.sarmiento@carejourney.com>
2025-04-06 01:41:23 -0300

    added back link to work in txt on header

--- apps/delivery-web/src/components/Header/ProductsHeader.tsx
+++ apps/delivery-web/src/components/Header/ProductsHeader.tsx
@@ -125,6 +125,7 @@ export function ProductsHeader({
           </ActionIcon>
         </Box>
         <Text
+          onClick={onBackClick}
           style={{
             fontFamily: 'Inter, sans-serif',
             fontStyle: 'normal',
