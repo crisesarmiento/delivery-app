cc54df0b13f1f2eb4fa76350ddca5aa74f5168b2
Cristian Sarmiento <cristian.sarmiento@carejourney.com>
2025-04-09 00:46:10 -0300

    fixed good practice of viewport for next

--- apps/delivery-web/src/app/layout.tsx
+++ apps/delivery-web/src/app/layout.tsx
@@ -1,14 +1,19 @@
-import type { Metadata } from 'next';
+import type { Metadata, Viewport } from 'next';
 import { Inter } from 'next/font/google';
 import ClientLayout from './ClientLayout';
 import './global.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+};
+
 export const metadata: Metadata = {
   title: 'Smarty Delivery',
   description: 'Plataforma de pedidos de comida online',
-  viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
 };
 
 export default function RootLayout({
